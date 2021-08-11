6c562e24905d0b320a9426c92d2c0776a2162fb9
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-08-11 08:39:37 -0600

    static

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.use("/public", express.static(path.join(__dirname, "../public")));
+app.use("/static", express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
